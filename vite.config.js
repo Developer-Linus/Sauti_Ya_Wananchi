0efe2e780f125f3e8877765c42b99d73596c6cdc
Developer-Linus <linusmksu30@gmail.com>
2024-10-07 15:09:13 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Sauti_Ya_Wananchi/',
+  base: process.env.NODE_ENV === 'production' ? '/Sauti_Ya_Wananchi/' : '/',
 });
